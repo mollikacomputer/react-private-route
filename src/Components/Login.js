bc339f7a29e172e7810bcb2327fc9c5b7215676f
mollikacomputer <mollikacomputer3@gmail.com>
2022-11-02 23:54:58 +0600

    authintaction some update

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {getAuth, signInWithPopup} from "firebase/auth";
+const auth = getAuth(app);
 
 const Login = () => {
+    const [user, setUser] = useState('');
+    const [email, setEmail] = useState('');
+    const handleGoogleSignIn = () => {
+        const googleProvider = new GoogleAuthProvider();
+        signInWithPopup(auth, googleProvider)
+        .then((result) =>{
+            const user = result.user;
+            setUser(user.displayName);
+            setEmail(user.email);
+            console.log(user.displayName, user.email);
+        })
+    }
     return (
         <div>
             <h2 className='text-3xl' > Log In </h2>
+            <button onClick={handleGoogleSignIn} > Sign In with google </button>
+            <h2>Name : <i> {user}</i></h2>
+            <h2>Email : <i> {email}</i></h2>
         </div>
     );
 };
